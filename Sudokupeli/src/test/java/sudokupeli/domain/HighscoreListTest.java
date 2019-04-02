8b2c2cd3838ba29d5aad094e950f653d5c0c147a
tomih <tomi.haapakoski@helsinki.fi>
2019-04-02 16:49:33 +0300

    Kansiorakennetta selkeytetty

--- Sudokupeli/src/test/java/sudokupeli/domain/HighscoreListTest.java
+++ Sudokupeli/src/test/java/sudokupeli/domain/HighscoreListTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package domain;
+package sudokupeli.domain;
 
 import java.io.File;
 import java.io.FileWriter;
