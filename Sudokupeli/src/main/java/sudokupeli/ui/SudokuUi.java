b39ecc81679e049b1d18c8155ed96954e0771192
tomih <tomi.haapakoski@helsinki.fi>
2019-04-09 18:43:30 +0300

    Lisätty sudokunäkymään varsinainen sudokun näyttävä toiminnalisuus ja nappula, joka tarkistaa sudokun

--- Sudokupeli/src/main/java/sudokupeli/ui/SudokuUi.java
+++ Sudokupeli/src/main/java/sudokupeli/ui/SudokuUi.java
@@ -1,24 +1,24 @@
 package sudokupeli.ui;
 
+import java.awt.Color;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import sudokupeli.domain.Player;
+import sudokupeli.domain.Sudoku;
 
 public class SudokuUi extends Application {
 
     Player currentPlayer;
+    Sudoku sudoku;
 
     @Override
     public void start(Stage window) {
@@ -45,7 +45,6 @@ public class SudokuUi extends Application {
 
         BorderPane sudokuGroup = new BorderPane();
         sudokuGroup.setTop(placeholder);
-        
 
         Scene sudokuView = new Scene(sudokuGroup);
 
@@ -58,21 +57,54 @@ public class SudokuUi extends Application {
             }
             Player player = new Player(username);
             this.currentPlayer = player;
-            placeholder.setText(this.currentPlayer.getName() + ", sudokusi illmestyy tänne");
+            placeholder.setText("Pelaaja: " + this.currentPlayer.getName());
             window.setScene(sudokuView);
         });
-        
+
+        this.sudoku = new Sudoku();
+        this.sudoku.initializeSudoku();
+
         GridPane tiles = new GridPane();
-        for(int x = 1; x<=9; x++){
-            for(int y = 1; y<=9; y++){
+        for (int x = 1; x <= 9; x++) {
+            for (int y = 1; y <= 9; y++) {
                 Button tile = new Button(" ");
-                tile.setFont(Font.font("Monospaced",40));
+                tile.setFont(Font.font("Monospaced", 40));
+                tile.setUserData(Color.RED);
+                tile.setOnAction((event) ->{
+                    int yCoordinate = GridPane.getRowIndex(tile) -1;
+                    int xCoordinate = GridPane.getColumnIndex(tile) -1;
+                    if(sudoku.numberCanBeChanged(xCoordinate, yCoordinate)){
+                        int value = sudoku.raiseTileValue(xCoordinate, yCoordinate);
+                        tile.setText("" + value);
+                    }
+                 
+                });
+                int value = sudoku.getTileValue(x-1, y-1);
+                if (value == 0) {
+                    tile.setText(" ");
+                } else {
+                    tile.setText(""+ value);
+                    tile.setStyle("-fx-text-fill: red;");
+                }
                 tiles.add(tile, x, y);
-                
+
             }
         }
-        
+
         sudokuGroup.setCenter(tiles);
+        
+        Button checkAnswer = new Button("Tarkista");
+        sudokuGroup.setBottom(checkAnswer);
+        
+        checkAnswer.setOnAction((event) ->{
+            final boolean isCorrect = sudoku.solutionIsCorrect();
+            if(isCorrect){
+                placeholder.setText("Oikein, peli päättyy");
+            }else{
+                placeholder.setText("Väärin, yritä uudestaan");
+            }
+        
+        });
 
         window.setScene(LoginView);
         window.show();
