acad2b2cad5ee24ba0b7d1ccc144892f13900d5c
tomih <tomi.haapakoski@helsinki.fi>
2019-05-04 21:57:58 +0300

    Käyttöliittymä päivitetty

--- Sudokupeli/src/main/java/sudokupeli/ui/SudokuUi.java
+++ Sudokupeli/src/main/java/sudokupeli/ui/SudokuUi.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -43,28 +44,98 @@ public class SudokuUi extends Application {
 
     @Override
     public void start(Stage window) {
-
+        VBox welcomeGroup = new VBox(175);
+        Scene welcomeView = new Scene(welcomeGroup);
         this.sudoku = new Sudoku();
 
-        Label usernameLabel = new Label("Syötä nimimerkki");
+        welcomeGroup.setPrefSize(745, 780);
+        welcomeGroup.setPadding(new Insets(50, 260, 50, 260));
+        welcomeGroup.setStyle("-fx-background-color: white;");
+
+        Label welcomeTitle = new Label("Tervetuloa");
+        welcomeTitle.setFont(new Font(40));
+
+        Button showLogin = createMenuButton("Pelaa");
+        Button showStatistics = createMenuButton("Tilastot");
+
+        showLogin.setOnAction((event) -> {
+            window.setScene(createGameScene(window,welcomeView));
+
+        });
+        
+        showStatistics.setOnAction((event) ->{
+            window.setScene(createStatisticsScene(window,welcomeView));
+        
+        });
+
+        welcomeGroup.getChildren().add(welcomeTitle);
+        welcomeGroup.getChildren().add(showLogin);
+        welcomeGroup.getChildren().add(showStatistics);
+
+        window.setScene(welcomeView);
+        window.show();
+
+    }
+    
+    public Scene createStatisticsScene(Stage window, Scene welcomeView){
+        VBox statisticsGroup = new VBox(25);
+        statisticsGroup.setPrefSize(745, 780);
+        statisticsGroup.setPadding(new Insets(50, 175, 50, 175));
+        statisticsGroup.setStyle("-fx-background-color: white;");
+        
+        List<Player> ranking = this.highscores.asList();
+        
+        Label title = new Label("Ranking");
+        title.setFont(new Font(40));
+        statisticsGroup.getChildren().add(title);
+        
+        for(int i = 0; i<ranking.size(); i++){
+            Player player = ranking.get(i);
+            Label row = new Label((i+1) + " " +  player.getName() + " ajalla " + player.getFastestTime() + " sekuntia ");
+            row.setFont(new Font(20));
+            statisticsGroup.getChildren().add(row);
+        }
+        
+        Button backButton = createMenuButton("Takaisin");
+        backButton.setOnAction((event) ->{
+            window.setScene(welcomeView);
+        });
+        
+        statisticsGroup.getChildren().add(backButton);
+ 
+        return new Scene(statisticsGroup);
+    }
+    
+    public Scene createGameScene(Stage window, Scene welcomeView){
+        
+        Label loginTitle = new Label("Luo Peli");
+        loginTitle.setFont(Font.font(40));
+
         TextField usernameField = new TextField();
+        usernameField.setStyle("-fx-background-color: white;"
+                + "             -fx-border-color:black;"
+                + "             -fx-border-width: 1 1 1 1 ;");
 
         Slider difficultySlider = drawDifficultySlider();
 
         Button createUserButton = new Button("Sudokuun!");
+        createUserButton.setStyle("-fx-background-color: White;"
+                + "   -fx-border-color:black;");
+
         Label invalidUsernameLabel = new Label("");
 
-        GridPane loginGroup = new GridPane();
-        loginGroup.setPrefSize(600, 200);
-        loginGroup.add(usernameLabel, 0, 0);
-        loginGroup.add(usernameField, 1, 0);
-        loginGroup.add(difficultySlider, 1, 2);
-        loginGroup.add(createUserButton, 1, 3);
-        loginGroup.add(invalidUsernameLabel, 1, 4);
+        VBox loginGroup = new VBox(20);
+        loginGroup.setPrefSize(745, 780);
+        loginGroup.getChildren().add(loginTitle);
+        loginGroup.getChildren().add(new Label("Syötä nimimerkki"));
+        loginGroup.getChildren().add(usernameField);
+        loginGroup.getChildren().add(new Label("Valitse vaikeustaso"));
+        loginGroup.getChildren().add(difficultySlider);
+        loginGroup.getChildren().add(createUserButton);
+        loginGroup.getChildren().add(invalidUsernameLabel);
 
-        loginGroup.setHgap(20);
-        loginGroup.setVgap(20);
-        loginGroup.setPadding(new Insets(20, 20, 20, 20));
+        loginGroup.setPadding(new Insets(50, 50, 50, 50));
+        loginGroup.setStyle("-fx-background-color: white;");
 
         clock = new StopWatch();
 
@@ -73,24 +144,28 @@ public class SudokuUi extends Application {
         Label playerInfo = new Label("Sudoku ilmestyy tänne");
 
         BorderPane sudokuGroup = new BorderPane();
-        sudokuGroup.setTop(playerInfo);
+        sudokuGroup.setLeft(playerInfo);
+        sudokuGroup.setStyle("-fx-background-color: white;");
 
         Scene sudokuView = new Scene(sudokuGroup);
 
         createUserButton.setOnAction((ActionEvent event) -> {
             String username = usernameField.getText().trim();
+            usernameField.clear();
             int usernameLenght = username.length();
             if (usernameLenght < 1 || usernameLenght > 8) {
                 invalidUsernameLabel.setText("Käyttäjätunnuksen pitää olla 1-8 merkkiä");
                 return;
             }
             this.currentPlayer = new Player(username);
-            System.out.println(difficulty);
             this.sudoku.initializeSudoku(difficulty);
 
-            sudokuGroup.setCenter(drawSudoku());
+            sudokuGroup.setBottom(drawSudoku());
 
             window.setScene(sudokuView);
+
+            System.out.println(window.getWidth());
+            System.out.println(window.getHeight());
 
             clock.start();
             userTimeAnimator = new AnimationTimer() {
@@ -110,24 +185,37 @@ public class SudokuUi extends Application {
 
         });
 
-        VBox highScoreGroup = new VBox();
-        highScoreGroup.getChildren().add(new Label("Pelaajat jotka ovat ratkaisseet sudokun nopeimmin: "));
-        List<Player> players = this.highscores.asList();
-        for (int i = 0; i < players.size(); i++) {
-            highScoreGroup.getChildren().add(new Label(players.get(i).getName() + " ajalla " + players.get(i).getFastestTime() + " sekuntia"));
-        }
-
-        sudokuGroup.setRight(highScoreGroup);
-
         Button checkAnswer = new Button("Tarkista");
-        sudokuGroup.setBottom(checkAnswer);
+        checkAnswer.setStyle("-fx-background-color: White;"
+                + "   -fx-border-color:black;"
+                + "   -fx-text-fill: Green;");
 
+        Button concede = new Button("Luovuta");
+        concede.setStyle("-fx-background-color: White;"
+                + "   -fx-border-color:black;"
+                + "   -fx-text-fill: Red;");
+
+        HBox gamecontrols = new HBox();
+
+        gamecontrols.getChildren().add(checkAnswer);
+        gamecontrols.getChildren().add(concede);
+
+        sudokuGroup.setRight(gamecontrols);
+
+        concede.setOnAction((event) -> {
+            userTimeAnimator.stop();
+            clock.stop();
+            clock.reset();
+            window.setScene(welcomeView);
+
+        });
         checkAnswer.setOnAction((event) -> {
             final boolean isCorrect = sudoku.solutionIsCorrect();
             if (isCorrect) {
                 userTimeAnimator.stop();
                 int time = (int) clock.getTime() / 1000;
                 clock.stop();
+                clock.reset();
                 currentPlayer.setTime(time);
                 try {
                     this.highscores.addPlayer(currentPlayer);
@@ -135,15 +223,46 @@ public class SudokuUi extends Application {
                     System.out.println(e);
                 }
                 playerInfo.setText("Oikein, peli päättyy");
+                window.setScene(createCorrectAnswerScene(window,loginView));
             } else {
                 playerInfo.setText("Väärin, yritä uudestaan");
             }
 
         });
+        
+        return loginView;
+    }
 
-        window.setScene(loginView);
-        window.show();
+    public Scene createCorrectAnswerScene(Stage window, Scene nextView) {
+        VBox correctAnswerGroup = new VBox(175);
+        correctAnswerGroup.setPrefSize(745, 780);
+        correctAnswerGroup.setPadding(new Insets(50, 175, 50, 175));
+        correctAnswerGroup.setStyle("-fx-background-color: white;");
+        
+        Label welcomeTitle = new Label("Oikea Vastaus!");
+        welcomeTitle.setFont(new Font(40));
+        
+        Button playAgain = createMenuButton("Pelaa uudestaan");
+        
+        playAgain.setOnAction((event) ->{
+            window.setScene(nextView);
+        });
+        
+        correctAnswerGroup.getChildren().add(welcomeTitle);
+        correctAnswerGroup.getChildren().add(playAgain);
+        
 
+        return new Scene(correctAnswerGroup);
+    }
+
+    public Button createMenuButton(String title) {
+        Button button = new Button(title);
+        button.setFont(new Font(30));
+        button.setMaxSize(300, 50);
+        button.setStyle("-fx-background-color: white;"
+                + "   -fx-border-color:black;");
+
+        return button;
     }
 
     public GridPane drawSudoku() {
@@ -163,11 +282,27 @@ public class SudokuUi extends Application {
 
                 });
                 int value = sudoku.getTileValue(x - 1, y - 1);
+                String border = "1 1 1 1;";
+                if (x % 3 == 0 && x < 9) {
+                    border = "1 5 1 1;";
+                }
+                if (y % 3 == 0 && y < 9) {
+                    border = "1 1 5 1;";
+                }
+                if (x % 3 == 0 && y % 3 == 0 && y < 9 && x < 9) {
+                    border = "1 5 5 1;";
+                }
                 if (value == 0) {
                     tile.setText(" ");
+                    tile.setStyle("-fx-background-color: white;"
+                            + "   -fx-border-color:black;"
+                            + "   -fx-border-width: " + border
+                            + "   -fx-text-fill: blue;");
                 } else {
                     tile.setText("" + value);
-                    tile.setStyle("-fx-text-fill: red;");
+                    tile.setStyle("-fx-background-color: white;"
+                            + "   -fx-border-color:black;"
+                            + "   -fx-border-width: " + border);
                 }
                 tiles.add(tile, x, y);
 
